9f39829e61a8834f134a2bf5d8c72576516da1b5
SaqibUsman02 <tech54qi@gmail.com>
2023-05-16 01:10:06 +0500

    ww

--- src/components/AllReport.js
+++ src/components/AllReport.js
@@ -12,7 +12,7 @@ const AllReport = () => {
     const callAboutPage = async() => {
   
         try{
-            const res= await fetch('/FetchReport',{
+            const res= await fetch('https://audf-server.vercel.app/FetchReport',{
                 method:"GET",
                 headers: {
                     Accept: "application/json",
